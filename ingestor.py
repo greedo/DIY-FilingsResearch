7cc8cce42cd80a0f0e6e4cbb1e9562cdcd59ebec
Joe Cabrera <calcmaster16@gmail.com>
2014-12-21 17:19:38 -0500

    adding SEDAR function

--- ingestor.py
+++ ingestor.py
@@ -14,58 +14,8 @@ try:
 except ImportError:
     import StringIO
 
+
 class Ingestor():
-
-    def html_search(self, tree, types):
-
-        grab_next = False
-        tables = list(tree.iter("table"))
-
-        try:
-            for table in tables:
-                if table.attrib['summary'] == "Document Format Files":
-                    for row in table.findall('tr'):
-                        for column in row.findall('td'):
-                            if grab_next:
-                                links = list(column.iter("a"))
-                                for link in links:
-                                    return link.attrib['href']
-                                    break
-                                grab_next = False
-                            if column.text == types:
-                                grab_next = True
-        except UnicodeEncodeError:
-            pass
-
-    def ingest_stock(self, ticker):
-
-        doc_types = ['10-K', '10-Q']
-        to_parse = []
-        sec_root = "http://www.sec.gov"
-
-        for types in doc_types:
-            feed = requests.get('http://www.sec.gov/cgi-bin/browse-edgar', params={'action': 'getcompany', 'CIK': ticker, 'type': types, 'count': 200, 'output': 'atom'})
-
-            # iso-8859-1 -> utf-8
-            processed = feed.text.decode('iso-8859-1').encode('utf8')
-
-            try:
-                ticker_feed = ET.fromstring(processed)
-            except Exception, e:
-                break
-            root = ticker_feed
-
-            for item in ticker_feed.findall('{http://www.w3.org/2005/Atom}entry'):
-                html_url = item[1].find('{http://www.w3.org/2005/Atom}filing-href').text.encode('ascii','ignore')
-
-                output = StringIO.StringIO(requests.get(html_url).text.encode('ascii','ignore'))
-                tree = etree.parse(output, etree.HTMLParser())
-        
-                output = self.html_search(tree, types)
-                if output and ".htm" in output:
-                    to_parse.append(sec_root+output)
-
-        return to_parse
 
     def file_downloader(self, urls, directory):
 
@@ -95,3 +45,90 @@ class Ingestor():
 
                 except Exception as exc:
                     print('%r generated an exception: %s' % (url, exc))
+
+
+class SEDAR(object):
+
+    def __init__(self, start_date, end_date):
+        self.org_root = "http://www.sedar.com"
+        start_parts = start_date.split("-")
+        self.start_month = start_parts[0]
+        self.start_date = start_parts[1]
+        self.start_year = start_parts[2]
+
+        end_parts = end_date.split("-")
+        self.end_month = end_parts[0]
+        self.end_date = end_parts[1]
+        self.end_year = end_parts[2]
+
+    def ingest_stock(self, ticker):
+
+        to_parse = []
+
+        feed = requests.get(self.org_root+'/FindCompanyDocuments.do', params={'lang': 'EN', 'page_no': '1', 'company_search': stock, 'document_selection': 5, 'industry_group': 'A', 'FromMonth': self.start_month, 'FromDate': self.start_date, 'FromYear':  self.start_year, 'ToMonth': self.end_month, 'ToDate': self.end_date, 'ToYear': self.end_year, 'Variable': 'Issuer', 'Search': 'Search'})
+
+        # iso-8859-1 -> utf-8
+        processed = feed.text.decode('iso-8859-1').encode('utf8')
+
+        try:
+            ticker_feed = ET.fromstring(processed)
+        except Exception, e:
+            break
+        root = ticker_feed
+        print root
+
+
+class SEC(object):
+
+    def __init__(self, start_date, end_date):
+        self.org_root = "http://www.sec.gov"
+
+    def html_search(self, tree, types):
+
+        grab_next = False
+        tables = list(tree.iter("table"))
+
+        try:
+            for table in tables:
+                if table.attrib['summary'] == "Document Format Files":
+                    for row in table.findall('tr'):
+                        for column in row.findall('td'):
+                            if grab_next:
+                                links = list(column.iter("a"))
+                                for link in links:
+                                    return link.attrib['href']
+                                    break
+                                grab_next = False
+                            if column.text == types:
+                                grab_next = True
+        except UnicodeEncodeError:
+            pass
+
+    def ingest_stock(self, ticker):
+
+        doc_types = ['10-K', '10-Q']
+        to_parse = []
+
+        for types in doc_types:
+            feed = requests.get(self.org_root+'/cgi-bin/browse-edgar', params={'action': 'getcompany', 'CIK': ticker, 'type': types, 'count': 200, 'output': 'atom'})
+
+            # iso-8859-1 -> utf-8
+            processed = feed.text.decode('iso-8859-1').encode('utf8')
+
+            try:
+                ticker_feed = ET.fromstring(processed)
+            except Exception, e:
+                break
+            root = ticker_feed
+
+            for item in ticker_feed.findall('{http://www.w3.org/2005/Atom}entry'):
+                html_url = item[1].find('{http://www.w3.org/2005/Atom}filing-href').text.encode('ascii','ignore')
+
+                output = StringIO.StringIO(requests.get(html_url).text.encode('ascii','ignore'))
+                tree = etree.parse(output, etree.HTMLParser())
+        
+                output = self.html_search(tree, types)
+                if output and ".htm" in output:
+                    to_parse.append(self.org_root+output)
+
+        return to_parse
