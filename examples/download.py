964ccc2bb8111128282d3467d02d0ed12c0eefcf
Joe Cabrera <calcmaster16@gmail.com>
2014-12-29 18:05:06 -0500

    importing from ticker list

--- examples/download.py
+++ examples/download.py
@@ -16,6 +16,6 @@ if not os.path.exists(docs_directory):
     os.mkdir(docs_directory)
 
 # for every ticker in our input file, download all the relevant documents
-with open('data.txt', 'r') as reader:
+with open('tickers.txt', 'r') as reader:
     for line in reader:
         ingestor.file_downloader(edgar.ingest_stock(line.rstrip()), docs_directory)
