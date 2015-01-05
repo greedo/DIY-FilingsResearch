40dc977ac0fd241594907bde6a9f6a7b52c3e0ca
Joe Cabrera <calcmaster16@gmail.com>
2015-01-05 00:21:40 -0500

    practical sedar test

--- tests/test_download.py
+++ tests/test_download.py
@@ -2,18 +2,17 @@
 # encoding: utf-8
 
 from ingestor import Ingestor, Edgar, Sedar, IngestorException
+import requests, requests.utils
 import os
 import shutil
 import sys
 sys.path.insert(0, os.path.abspath('./'))
 import pytest
 
-from ingestor import Ingestor, Edgar, Sedar
-
 docs_directory = "test"
 
-ping = pytest.mark.skipif(os.system("ping -c 1 " + Edgar().org_root.replace("http://", "")) != 0,
-                          reason="could not reach Edgar")
+ping_edgar = pytest.mark.skipif(os.system("ping -c 1 " + Edgar().org_root.replace("http://", "")) != 0,
+                                reason="could not reach Edgar")
 
 def setup_module():
     """ create folder for downloading docs """
@@ -21,7 +20,7 @@ def setup_module():
     if not os.path.exists(docs_directory):
         os.mkdir(docs_directory)
 
-@ping
+@ping_edgar
 def test_edgar_download_html():
     
     ingestor = Ingestor()
@@ -30,7 +29,7 @@ def test_edgar_download_html():
 
     assert os.path.exists(docs_directory+"/a2124888z10-k.htm") == True
 
-@ping
+@ping_edgar
 def test_edgar_download_xbrl():
 
     ingestor = Ingestor()
@@ -46,6 +45,38 @@ def test_sedar_create_html():
 def test_sedar_create_xbrl():
     ingestor = Ingestor()
     assert Sedar("xbrl", "2010-01-01", "2014-01-01")
+
+def test_sedar_ingest_xbrl():
+    ingestor = Ingestor()
+    sedar = Sedar("xbrl", "2010-01-01", "2014-01-01")
+
+    headers = {
+        'User-Agent': 'DIY-FilingsResearch 0.1'
+    }
+
+    initial_params = {
+        'lang': 'EN',
+        'page_no': 1,
+        'company_search': 'blackberry',
+        'document_selection': 26,
+        'industry_group': 'A',
+        'FromMonth': 1,
+        'FromDate': 1,
+        'FromYear': 2010,
+        'ToMonth': 1,
+        'ToDate': 1,
+        'ToYear': 2014,
+        'Variable': 'Issuer',
+        'Search': 'Search'
+    }
+
+    session = requests.session()
+
+    initial_request = session.post(Sedar().org_root+"/FindCompanyDocuments.do", params=initial_params)
+    processed = initial_request.text.encode('utf-8')
+    cookies = requests.utils.dict_from_cookiejar(initial_request.cookies)
+
+    assert cookies['BIGipServerP_R1_sedar_80'] is not None
 
 def test_sedar_exception():
     ingestor = Ingestor()
