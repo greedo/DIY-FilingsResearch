e5d807971948c07cf72c92dad117108d6278f421
Joe Cabrera <calcmaster16@gmail.com>
2015-01-15 10:24:39 -0500

    edgar_tickers.txt, corrected setup

--- searcher.py
+++ searcher.py
@@ -1,1 +1,40 @@
-examples/searcher.py
+#! /usr/bin/env python
+# encoding: utf-8
+
+from threadedSearch import Queryer, Indexer
+from ingestor import Ingestor, Edgar, Sedar
+import signal
+import lucene
+
+# before we quit we always want to close the writer to prevent corruptions to the index
+def quit_gracefully(*args):
+    queryer.writer.close()
+    print "Cleaning up and terminating"
+    exit(0)
+
+# always declare the signal handler first
+signal.signal(signal.SIGINT, quit_gracefully)
+
+env=lucene.initVM()
+queryer = Queryer("index", "hits")
+print 'Using Directory: ', queryer.store_dir
+
+# directory for storing downloaded docs
+directoryToWalk = "docs"
+
+# and start the indexer
+# note the indexer thread is set to daemon causing it to terminate on a SIGINT
+indexer = Indexer(queryer.store_dir, queryer.writer, directoryToWalk)
+ingestor = Ingestor()
+edgar = Edgar()
+
+with open('edgar_tickers.txt', 'r') as reader:
+    for line in reader:
+        ingestor.file_downloader(edgar.ingest_stock(line.rstrip()), directoryToWalk)
+        indexer.indexDocs()
+
+# start up the terminal query interface
+queryer.run(queryer.writer, queryer.analyzer)
+
+# if return from Querying then call the signal handler to clean up the writer cleanly
+quit_gracefully()
--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ if os.path.exists('README.rst'):
 
 setup(
     name='DIY-FilingsResearch',
-    version='0.0.1',
+    version='0.2.0',
     description='library for analyzing company financial documents',
     author='Joe Cabrera',
     author_email='jcabrera@eminorlabs.com',
     url='https://github.com/greedo/DIY-FilingsResearch/',
     license='Apache License',
     keywords='Financial, Accounting, file formats, 10k, 10q, filings, edgar, sedar',
-    scripts=['ingestor.py', 'examples/download.py'],
-    install_requires=['requests', 'lxml'],
+    scripts=['threadedSearch.py', 'ingestor.py', 'examples/searcher.py', 'examples/download_edgar.py', 'examples/download_sedar.py'],
+    install_requires=['pytest', 'requests', 'lxml', 'futures', 'selenium'],
     classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: English',
